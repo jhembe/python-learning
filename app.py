ff923f6b9efb52c4bcbe9f5ec5953beb56a6ccd9
Joseph Gabriel <57839839+jhembe@users.noreply.github.com>
2021-06-28 14:57:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+# print("Joseph Gabriel")
+# print('o----')
+# print(' ||||')
+# print('*'*10)
+
+# price = 10
+# price = 30
+# rating = 7.5
+# name = "Joseph"
+# isHuman = True
+# print(price)
+# print(rating)
+# print(name)
+# print(isHuman)
+
+# full_name ="John Smith"
+# age = 20
+# isNew = True
+
+#how to accept input from the user
+
+
+# mname = input("Enter your name : ")
+# print("welcome back "+mname)
+
+# sname = input("What's your name : ")
+# scolor = input("What's your favourite color : ")
+# print(sname + " likes " + scolor + " color !!")
+
+# birth_date = input("Birth Date : ")
+# age = 2021 - int(birth_date)
+# print("you are "+ str(age) + " years old")
+# print("the type of age is a "+ str(type(age)))
+
+#2.2 pounds
+
+# weight = input("Enter the weight in pounds ")
+# print("The weight in Kilogram is " + str(float(weight)/2.2046))
+
+#course = "Python's course for beginers";
+#print(course);
+
+# course = '''Please read this thingy
+# here is the real deal
+# I am studying python
+# and there is the best 
+# thingy someone will know 
+# about lying and eating coconut
+# trees
+
+# '''
+
+# print(course)
+# print(course[0])
+# print(course[10])
+# print(course[-2])
+# print(course[7:])
+# print(course[0:3])
+
+# another = course [:]
+# print(another)
+
+# name = "jennifer"
+# print(name[1:-1])
+
+# first = "John"
+# last = "Smith"
+# message = first +" ["+last+"] "+"is a coder ";
+# msg = f'{first} [{last}] is a coder ' #this is called a formated string
+# msging =f'{first} is my name' #this is called a formated string 
+# print(message)
+# print(msg)
+# print(msging)
+
+#printing the length of the string stored in course;
+# print(len(course));
+# print(course.upper())
+# print(course.lower())
+
+#to find something we use the the method .find()
+# print(course.find("H"))
+# print(course.find("h"))
+
+# we can alse replace a sequence of characters, so to do that, we use the method : method
+# .replace("original_string","replacing string")
+# lets find first..
+# print(course.find("am"))
+# print(course.replace("am","is"))
+#this replace method, can also work in characters, not only in words
+
+#we can also check the existance of the characters in a variable using "in" operator
+#it normally returns a bolean value
+# print("is" in course)
+#it is case sensitive, hence changing in uper case
+# print("Is" in course)
+# print(course.title())
+# find method returns the index of the character of the stirng variable..
+# while the "in" operator, returns the boolean value if either the searched word is present ...
+# 
+
+#len()
+# .upper()
+# .lower() 
+#.find
+# "string" in
+#  
+
+
