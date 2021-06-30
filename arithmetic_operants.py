f18775c8849ac614800502fe1d5b547c4609bb13
Joseph Gabriel <57839839+jhembe@users.noreply.github.com>
2021-06-30 09:35:18 +0300

    Add files via upload

---
+++ arithmetic_operants.py
@@ -0,0 +1,37 @@
+# Arithmetic Operation
+
+# print(5+2)
+# print(5-2)
+
+# print(10/3)
+# #but the above division operator, produces lots of decimal places
+# #to approximate we use double foward slash
+# print(10//3)
+# # For modulus we use
+# print(10%3) #hence produces a remainder
+# # to use power, then we use double asterisk **
+# print(10 ** 3)
+
+# x = 10
+# x += 3
+# #or
+# x = x + 3
+# print(x)
+
+#using the round function --we use the built in function round()
+#consider:
+m = 0.7786
+rounded = round(m)
+print(rounded)
+
+#using the absolute function -- we use the built in function abs()
+#consider:
+print(abs(-m))  
+
+import math
+
+print(math.ceil(2.8)) #provides the approximation by the nearest number of the decimal point
+print(math.floor(2.8)) #provides the integer thats not associated with the decimal
+
+
+
